a952ca9bee2d27fd10e23f5e43474ab6539871a0
Derek Reidy <50381777+DerekJR321@users.noreply.github.com>
2019-10-25 00:38:27 -0400

    Add files via upload

---
+++ hockey.sql
@@ -0,0 +1,505 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 25, 2019 at 06:37 AM
+-- Server version: 10.4.6-MariaDB
+-- PHP Version: 7.3.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hockey`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `country`
+--
+
+CREATE TABLE `country` (
+  `id` int(11) NOT NULL,
+  `name` text COLLATE utf8_unicode_ci NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `country`
+--
+
+INSERT INTO `country` (`id`, `name`, `status`) VALUES
+(1, 'Afghanistan', 1),
+(2, 'Albania', 1),
+(3, 'Algeria', 1),
+(4, 'Andorra', 1),
+(5, 'Angola', 1),
+(6, 'Antigua and Barbuda', 1),
+(7, 'Argentina', 1),
+(8, 'Armenia', 1),
+(9, 'Australia', 1),
+(10, 'Austria', 1),
+(11, 'Azerbaijan', 1),
+(12, 'Bahamas', 1),
+(13, 'Bahrain', 1),
+(14, 'Bangladesh', 1),
+(15, 'Barbados', 1),
+(16, 'Belarus', 1),
+(17, 'Belgium', 1),
+(18, 'Belize', 1),
+(19, 'Benin', 1),
+(20, 'Bhutan', 1),
+(21, 'Bolivia', 1),
+(22, 'Bosnia and Herzegovina', 1),
+(23, 'Botswana', 1),
+(24, 'Brazil', 1),
+(25, 'Brunei', 1),
+(26, 'Bulgaria', 1),
+(27, 'Burkina Faso', 1),
+(28, 'Burundi', 1),
+(29, 'Cabo Verde', 1),
+(30, 'Cambodia', 1),
+(31, 'Cameroon', 1),
+(32, 'Canada', 1),
+(33, 'Central African Republic', 1),
+(34, 'Chad', 1),
+(35, 'Chile', 1),
+(36, 'China', 1),
+(37, 'Colombia', 1),
+(38, 'Comoros', 1),
+(39, 'Congo, Republic of the', 1),
+(40, 'Congo, Democratic Republic of the', 1),
+(41, 'Costa Rica', 1),
+(42, 'Cote d Ivoire', 1),
+(43, 'Croatia', 1),
+(44, 'Cuba', 1),
+(45, 'Cyprus', 1),
+(46, 'Czech Republic', 1),
+(47, 'Denmark', 1),
+(48, 'Djibouti', 1),
+(49, 'Dominica', 1),
+(50, 'Dominican Republic', 1),
+(51, 'Ecuador', 1),
+(52, 'Egypt', 1),
+(53, 'El Salvador', 1),
+(54, 'Equatorial Guinea', 1),
+(55, 'Eritrea', 1),
+(56, 'Estonia', 1),
+(57, 'Ethiopia', 1),
+(58, 'Fiji', 1),
+(59, 'Finland', 1),
+(60, 'France', 1),
+(61, 'Gabon', 1),
+(62, 'Gambia', 1),
+(63, 'Georgia', 1),
+(64, 'Germany', 1),
+(65, 'Ghana', 1),
+(66, 'Greece', 1),
+(67, 'Grenada', 1),
+(68, 'Guatemala', 1),
+(69, 'Guinea', 1),
+(70, 'Guinea-Bissau', 1),
+(71, 'Guyana', 1),
+(72, 'Haiti', 1),
+(73, 'Honduras', 1),
+(74, 'Hungary', 1),
+(75, 'Iceland', 1),
+(76, 'India', 1),
+(77, 'Indonesia', 1),
+(78, 'Iran', 1),
+(79, 'Iraq', 1),
+(80, 'Ireland', 1),
+(81, 'Italy', 1),
+(82, 'Jamaica', 1),
+(83, 'Japan', 1),
+(84, 'Jordan', 1),
+(85, 'Kazakhstan', 1),
+(86, 'Kenya', 1),
+(87, 'Kiribati', 1),
+(88, 'Kosovo', 1),
+(89, 'Kuwait', 1),
+(90, 'Kyrgyzstan', 1),
+(91, 'Laos', 1),
+(92, 'Latvia', 1),
+(93, 'Lebanon', 1),
+(94, 'Lesotho', 1),
+(95, 'Liberia', 1),
+(96, 'Libya', 1),
+(97, 'Liechtenstein', 1),
+(98, 'Lithuania', 1),
+(99, 'Luxembourg', 1),
+(100, 'Macedonia', 1),
+(101, 'Madagascar', 1),
+(102, 'Malawi', 1),
+(103, 'Malaysia', 1),
+(104, 'Maldives', 1),
+(105, 'Mali', 1),
+(106, 'Malta', 1),
+(107, 'Marshall Islands', 1),
+(108, 'Mauritania', 1),
+(109, 'Mauritius', 1),
+(110, 'Mexico', 1),
+(111, 'Micronesia', 1),
+(112, 'Moldova', 1),
+(113, 'Monaco', 1),
+(114, 'Mongolia', 1),
+(115, 'Montenegro', 1),
+(116, 'Morocco', 1),
+(117, 'Mozambique', 1),
+(118, 'Myanmar (Burma)', 1),
+(119, 'Namibia', 1),
+(120, 'Nauru', 1),
+(121, 'Nepal', 1),
+(122, 'Netherlands', 1),
+(123, 'New Zealand', 1),
+(124, 'Nicaragua', 1),
+(125, 'Niger', 1),
+(126, 'Nigeria', 1),
+(127, 'North Korea', 1),
+(128, 'Norway', 1),
+(129, 'Oman', 1),
+(130, 'Pakistan', 1),
+(131, 'Palau', 1),
+(132, 'Palestine', 1),
+(133, 'Panama', 1),
+(134, 'Papua New Guinea', 1),
+(135, 'Paraguay', 1),
+(136, 'Peru', 1),
+(137, 'Philippines', 1),
+(138, 'Poland', 1),
+(139, 'Portugal', 1),
+(140, 'Qatar', 1),
+(141, 'Romania', 1),
+(142, 'Russia', 1),
+(143, 'Rwanda', 1),
+(144, 'St. Kitts and Nevis', 1),
+(145, 'St. Lucia', 1),
+(146, 'St. Vincent and The Grenadines', 1),
+(147, 'Samoa', 1),
+(148, 'San Marino', 1),
+(149, 'Sao Tome and Principe', 1),
+(150, 'Saudi Arabia', 1),
+(151, 'Senegal', 1),
+(152, 'Serbia', 1),
+(153, 'Seychelles', 1),
+(154, 'Sierra Leone', 1),
+(155, 'Singapore', 1),
+(156, 'Slovakia', 1),
+(157, 'Slovenia', 1),
+(158, 'Solomon Islands', 1),
+(159, 'Somalia', 1),
+(160, 'South Africa', 1),
+(161, 'South Korea', 1),
+(162, 'South Sudan', 1),
+(163, 'Spain', 1),
+(164, 'Sri Lanka', 1),
+(165, 'Sudan', 1),
+(166, 'Suriname', 1),
+(167, 'Swaziland', 1),
+(168, 'Sweden', 1),
+(169, 'Switzerland', 1),
+(170, 'Syria', 1),
+(171, 'Taiwan', 1),
+(172, 'Tajikistan', 1),
+(173, 'Tanzania', 1),
+(174, 'Thailand', 1),
+(175, 'Timor-Leste', 1),
+(176, 'Togo', 1),
+(177, 'Tonga', 1),
+(178, 'Trinidad and Tobago', 1),
+(179, 'Tunisia', 1),
+(180, 'Turkey', 1),
+(181, 'Turkmenistan', 1),
+(182, 'Tuvalu', 1),
+(183, 'Uganda', 1),
+(184, 'Ukraine', 1),
+(185, 'United Arab Emirates', 1),
+(186, 'United Kingdom (UK)', 1),
+(187, 'United States of America (USA)', 1),
+(188, 'Uruguay', 1),
+(189, 'Uzbekistan', 1),
+(190, 'Vanuatu', 1),
+(191, 'Vatican City (Holy See)', 1),
+(192, 'Venezuela', 1),
+(193, 'Vietnam', 1),
+(194, 'Yemen', 1),
+(195, 'Zambia', 1),
+(196, 'Zimbabwe', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `modules`
+--
+
+CREATE TABLE `modules` (
+  `id` tinyint(5) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `show` enum('0','1') NOT NULL,
+  `read` enum('0','1') NOT NULL,
+  `update` enum('0','1') NOT NULL,
+  `delete` enum('0','1') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `modules`
+--
+
+INSERT INTO `modules` (`id`, `name`, `show`, `read`, `update`, `delete`) VALUES
+(1, 'Admin Page', '1', '1', '1', '0'),
+(2, 'User Management', '1', '1', '0', '0');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `players`
+--
+
+CREATE TABLE `players` (
+  `id` int(5) NOT NULL,
+  `player_name` varchar(200) NOT NULL,
+  `player_number` int(2) NOT NULL,
+  `player_position` varchar(50) NOT NULL,
+  `player_shoots` varchar(1) NOT NULL,
+  `player_height` varchar(5) NOT NULL,
+  `player_weight` int(3) NOT NULL,
+  `player_dob` date NOT NULL,
+  `player_img` varchar(255) NOT NULL,
+  `team_id` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `players`
+--
+
+INSERT INTO `players` (`id`, `player_name`, `player_number`, `player_position`, `player_shoots`, `player_height`, `player_weight`, `player_dob`, `player_img`, `team_id`) VALUES
+(1, 'Josh Bailey', 12, 'C', 'L', '6\'1', 212, '1989-10-02', 'https://nhl.bamcontent.com/images/headshots/current/168x168/8474573.jpg', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `settings`
+--
+
+CREATE TABLE `settings` (
+  `settings_id` int(11) NOT NULL,
+  `type` longtext CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `description` longtext CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `settings`
+--
+
+INSERT INTO `settings` (`settings_id`, `type`, `description`) VALUES
+(1, 'system_name', 'Codeigniter Admin Template'),
+(2, 'system_title', 'Codeigniter Admin Template');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `teams`
+--
+
+CREATE TABLE `teams` (
+  `id` int(5) NOT NULL,
+  `teamName` varchar(200) NOT NULL,
+  `teamLogo` varchar(255) NOT NULL,
+  `teamManager` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `teams`
+--
+
+INSERT INTO `teams` (`id`, `teamName`, `teamLogo`, `teamManager`) VALUES
+(1, 'NY Islanders', 'https://upload.wikimedia.org/wikipedia/en/thumb/4/42/Logo_New_York_Islanders.svg/220px-Logo_New_York_Islanders.svg.png', 'Barry Trotz');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `mobile` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `country` int(11) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `status` int(11) NOT NULL,
+  `role` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `first_name`, `last_name`, `email`, `mobile`, `password`, `country`, `created_at`, `status`, `role`) VALUES
+(1, 'Super', 'Admin', 'admin1@admin.com', '0100 500 600', '8d788385431273d11e8b43bb78f3aa41', 14, '2018-03-02 14:51:38', 1, 'admin'),
+(16, 'Esther', 'User', 'teacher@teacher.com', '08053433', '8d788385431273d11e8b43bb78f3aa41', 11, '2019-02-27 00:10:25', 0, 'user'),
+(17, 'New Admin', 'New Admin', 'admin@admin.com', '08053433', '81dc9bdb52d04dc20036dbd8313ed055', 126, '2019-02-27 23:51:25', 1, 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_power`
+--
+
+CREATE TABLE `user_power` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `power_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `user_power`
+--
+
+INSERT INTO `user_power` (`id`, `name`, `power_id`) VALUES
+(2, 'edit', 2),
+(3, 'delete', 3),
+(4, 'add', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_role`
+--
+
+CREATE TABLE `user_role` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `action` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `user_role`
+--
+
+INSERT INTO `user_role` (`id`, `user_id`, `action`) VALUES
+(1, 11, 1),
+(2, 11, 3),
+(3, 13, 2),
+(4, 14, 1),
+(5, 14, 3),
+(6, 15, 1),
+(7, 15, 3),
+(11, 16, 1),
+(12, 16, 2);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `country`
+--
+ALTER TABLE `country`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `modules`
+--
+ALTER TABLE `modules`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `players`
+--
+ALTER TABLE `players`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `settings`
+--
+ALTER TABLE `settings`
+  ADD PRIMARY KEY (`settings_id`);
+
+--
+-- Indexes for table `teams`
+--
+ALTER TABLE `teams`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_power`
+--
+ALTER TABLE `user_power`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_role`
+--
+ALTER TABLE `user_role`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `country`
+--
+ALTER TABLE `country`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=197;
+
+--
+-- AUTO_INCREMENT for table `modules`
+--
+ALTER TABLE `modules`
+  MODIFY `id` tinyint(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `settings`
+--
+ALTER TABLE `settings`
+  MODIFY `settings_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=68;
+
+--
+-- AUTO_INCREMENT for table `teams`
+--
+ALTER TABLE `teams`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `user_power`
+--
+ALTER TABLE `user_power`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `user_role`
+--
+ALTER TABLE `user_role`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
